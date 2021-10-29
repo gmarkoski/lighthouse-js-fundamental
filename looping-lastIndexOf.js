8db5233058053c719fdde8d0f3f08546c3da0c7c
gmarkoski <gary_markoski@hotmail.com>
2021-10-29 16:34:52 -0600

    looping-lastIndexOf.js

--- looping-lastIndexOf.js
+++ looping-lastIndexOf.js
@@ -1,4 +1,4 @@
-function lastIndexOf(array, value) {
+function lastIndexOf(array, value) 
   for (let i = array.length - 1; i >= 0; i--) {
     if (array[i] === value) {
       return i;
@@ -6,7 +6,7 @@ function lastIndexOf(array, value) {
   }  
   return -1; 
 }
-    
+
 console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
 console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
 console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
